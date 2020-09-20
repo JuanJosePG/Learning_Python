d329ead57b7218ec572f866201dfda2ab14449f6
Juan José <66780299+JuanJosePG@users.noreply.github.com>
2020-09-20 18:22:45 +0200

    Create dicts_vs_lists.py

---
+++ dictionaries/dicts_vs_lists.py
@@ -0,0 +1,21 @@
+# Comparision between list and dictionaries
+# LIST
+l=list()
+l.append(2)
+l.append(23)
+l.append(123)
+print(l)
+
+l[2]=444
+print(l)
+
+
+# DICT
+d=dict()
+d["age"]=25
+d["course"]='A'
+d["year"]=2020
+print(d)
+
+d["year"]=1994
+print(d)
