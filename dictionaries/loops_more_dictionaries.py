90001814a2e4120c047540276736d06145a5bfc1
Juan José <66780299+JuanJosePG@users.noreply.github.com>
2020-09-20 18:02:51 +0200

    Create loops_more_dictionaries.py

---
+++ dictionaries/loops_more_dictionaries.py
@@ -0,0 +1,7 @@
+# For loops with two variables + dictionaries
+
+# The method .items() return a list with all dictionary keys and values
+    # dic={"dec": 12, "nov": 11, "sept": 9, "jan": 1}
+
+    # for k,v in dic.items():
+    #     print(k,v)
